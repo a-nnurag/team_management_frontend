fef930af2a2bb1b8ba540f72383ab6dd977ad9f8
unknown <asc15.lko@gmail.com>
2025-06-22 22:23:29 +0530

    case senstive issue resolved in inviteUser

--- src/routes/common/routes.jsx
+++ src/routes/common/routes.jsx
@@ -7,7 +7,7 @@ import ProjectDetails from "@/page/workspace/ProjectDetails";
 import { Settings } from "@/page/workspace/Settings";
 import Tasks from "@/page/workspace/Tasks";
 import { AUTH_ROUTES, BASE_ROUTE, PROTECTED_ROUTES } from "./routePaths";
-import InviteUser from "@/page/invite/inviteUser";
+import InviteUser from "@/page/invite/InviteUser";
 
 export const authenticationRoutePaths = [
   { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
